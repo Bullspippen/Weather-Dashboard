23bb132838bd13aeb7913b4ccf4bb28f16b671dc
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-30 19:15:26 -0500

    comments placed on script.js so as to compartmentalize

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+/* HTML ELEMENT INTERACTIONS
+
+Require the following
+Input element for searching, search button (addeventlistener)
+When the button is clicked, the search values that are displayed based on the input
+
+Forecast Section
+
+document.getElementById("forecast")
+
+Divs, class "card" within forecast section
+.querySelectorAll(".card")
+
+Loop through each of the "card" for 5 day forecast
