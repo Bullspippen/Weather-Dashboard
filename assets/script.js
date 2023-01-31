45ff13e611d9f8300622038ddc17728ceb76267f
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-30 19:37:58 -0500

    additional comments placed on script.js

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,26 @@ Divs, class "card" within forecast section
 .querySelectorAll(".card")
 
 Loop through each of the "card" for 5 day forecast
+
+let forecast = [day1, day2, day3, day4, day5]
+for(let i = 0; i < forecast.length; i++){
+let currentDay = forecast[i]
+let forecastDay = document.getElementById(`forecast-day${i+1}`);
+
+include card-title, card-text
+}
+
+
+
+
+ let currentDate = dayjs().format("MMMM D, YYYY");
+  $("#currentDay").text(currentDate);
+  let main = $("#main")
+  let hour = dayjs().get('hour')
+
+  let workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+
+  function workDayScheduler(){
+    for(let i = 0; i < workHours.length; i++){
+      console.log(hour)
+      let saveinput = localStorage.getItem(`${i}`)w]
